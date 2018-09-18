bda710dbf20b3c07e28f29e70c73063332b9e1bd
Coenfusion <coenvdberge@outlook.com>
2018-09-18 11:21:32 +0200

    ASP Project created

---
+++ RipoffExpress/RipoffExpress/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RipoffExpress.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
